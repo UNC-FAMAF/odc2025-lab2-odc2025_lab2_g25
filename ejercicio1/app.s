3aeb88b602b3286464a69964f0042a32d108e8b1
Alejo-manu <alejo.garcia.544@mi.unc.edu.ar>
2025-06-01 14:18:37 -0300

    Add files via upload
    cambio de cielo, agregar nubes, luna y cartel con OdC 2025 (faltan detalles para que parezca un cartel de publicidad) y se completo el gato

--- ejercicio1/app.s
+++ ejercicio1/app.s
@@ -1,194 +1,420 @@
-.equ SCREEN_WIDTH, 		640
+	.equ SCREEN_WIDTH, 		640
 	.equ SCREEN_HEIGH, 		480
 	.equ BITS_PER_PIXEL,  	32
-
-	.equ GPIO_BASE,      0x3f200000
-	.equ GPIO_GPFSEL0,   0x00
-	.equ GPIO_GPLEV0,    0x34
-
 	.globl main
 
 main:
-	// x0 contiene la direccion base del framebuffer
- 	mov x20, x0	// Guarda la dirección base del framebuffer en x20
-	//---------------- CODE HERE ------------------------------------
-	
+ 	mov x20, x0	
 	mov x21,SCREEN_WIDTH
 	mov x22,SCREEN_HEIGH
-	
+//-----------------------------------------------------------------------------------------------------------------------------------------
+	//cielo
 	movz w12, 0x00, lsl 16
-	movk w12, 0x0100, lsl 00
-	bl fondo   // 000000000000000011111111
-
-	   // Color del semicírculo (rojo: 0xFFFF0000)
-    movz w12, 0xff, lsl 16    // Componente rojo
-    movk w12, 0x8000, lsl 00      // Alpha
-    
-    // Parámetros del semicírculo
-    //mov x3, 100                 // Radio del semicírculo
-    mov x0, (SCREEN_WIDTH/2)-20      // Centro X (320)
-    mov x1, (SCREEN_HEIGH/2)      // Centro Y (240)
-	mov x2, 90
-	mov x3, 100
+	movk w12, 0x0033, lsl 00
+	movz w13, 0x00, lsl 16
+	movk w13, 0x0044, lsl 00
+	bl fondo
+//-----------------------------------------------------------------------------------------------------------------------------------------
+	//luna
+	movz x29, 0xFF, lsl 16
+	movk x29, 0xFF66, lsl 00
+	mov x0, 150   //x
+	mov x1, 200   //y
+	mov x2, 100   //radio
+	bl circulo
+//-----------------------------------------------------------------------------------------------------------------------------------------
+	//nube1
+	movz w12, 0x66, lsl 16
+	movk w12, 0x66CC, lsl 00
+	mov x0, 0  //x
+	mov x1, 60   //y
+	mov x2, 200   //ancho
+	mov x3, 40    //alto
 	bl cuadrado
 
-	mov x0, (SCREEN_WIDTH/2)-40      // Centro X (320)
-    mov x1, (SCREEN_HEIGH/2)+20      // Centro Y (240)
-	mov x2, 30
-	mov x3, 80
-	bl cuadrado
-	mov x0, (SCREEN_WIDTH/2)-60      // Centro X (320)
-    mov x1, (SCREEN_HEIGH/2)+40      // Centro Y (240)
-	mov x2, 30
-	mov x3, 80
+	mov x0, 40  
+	mov x1, 40   
+	mov x2, 100   
+	mov x3, 40    
 	bl cuadrado
 
-
-
-	//edificio 1
+	mov x0, 120  
+	mov x1, 100   
+	mov x2, 60   
+	mov x3, 10    
+	bl cuadrado
+	
+	//nube2
+	mov x0, 300  
+	mov x1, 160   
+	mov x2, 200   
+	mov x3, 40    
+	bl cuadrado
+	
+	mov x0, 400  
+	mov x1, 120   
+	mov x2, 100   
+	mov x3, 40    
+	bl cuadrado
+//---------------------------------------------------------------------------------------------------------------------------------------
+	//edificio 1-----------------------------------
 	movz w12, 0x00, lsl 16
 	movk w12, 0x0000, lsl 00
-	mov x0, 0   //x
-	mov x1, 320   //y
-	mov x2, 200   //ancho
-	mov x3, 160    //alto
-	bl cuadrado
-	
-	//gato
-	mov x0, 140   //x
-	mov x1, 260  //y
-	mov x2, 40   //ancho
-	mov x3, 20    //alto
+	mov x0, 0   
+	mov x1, 320   
+	mov x2, 200   
+	mov x3, 160    
 	bl cuadrado
 
 	//edificio 2
-	mov x0, 240   //x
-	mov x1, 360   //y
-	mov x2, 200   //ancho
-	mov x3, 120    //alto
-	bl cuadrado
-
-	//edificio 3
-	mov x0, 520   //x
-	mov x1, 0   //y
-	mov x2, 120   //ancho
-	mov x3, 480    //alto
+	mov x0, 240   
+	mov x1, 360   
+	mov x2, 200   
+	mov x3, 120    
 	bl cuadrado
 	
-	//techo 1
-	mov x0, 0   //x
-	mov x1, 280   //y
-	mov x2, 220   //ancho
-	mov x3, 40    //alto
+
+	//edificio 3
+	mov x0, 520   
+	mov x1, 0   
+	mov x2, 120   
+	mov x3, 480    
 	bl cuadrado
 
-	//techo 2
-	mov x0, 220   //x
-	mov x1, 340   //y
-	mov x2, 240   //ancho
-	mov x3, 40    //alto
-	bl cuadrado
-
-  //balcon 1
-	mov x0, 480   //x
-	mov x1, 0   //y
-	mov x2, 40   //ancho
-	mov x3, 40    //alto
+  	//balcon 1
+	mov x0, 480   
+	mov x1, 0   
+	mov x2, 40   
+	mov x3, 40    
 	bl cuadrado
 	
 	//balcon 2
-	mov x0, 480   //x
-	mov x1, 160   //y
-	mov x2, 40   //ancho
-	mov x3, 40    //alto
+	mov x0, 480   
+	mov x1, 160   
+	mov x2, 40   
+	mov x3, 40    
 	bl cuadrado
-
+	
+	mov x0, 480   
+	mov x1, 151   
+	mov x2, 3   
+	mov x3, 9    
+	bl cuadrado
+	
+	mov x0, 480   
+	mov x1, 151   
+	mov x2, 40   
+	mov x3, 5    
+	bl cuadrado
+	
 	//balcon 3
-	mov x0, 480   //x
-	mov x1, 320   //y
-	mov x2, 40   //ancho
-	mov x3, 40    //alto
+	mov x0, 480   
+	mov x1, 320   
+	mov x2, 40   
+	mov x3, 40    
 	bl cuadrado
 
-	//ventana
+	mov x0, 480   
+	mov x1, 311   
+	mov x2, 3   
+	mov x3, 9    
+	bl cuadrado
+	
+	mov x0, 480   
+	mov x1, 311   
+	mov x2, 40   
+	mov x3, 5    
+	bl cuadrado
+
+	//techo 1
+	mov x0, 0   
+	mov x1, 280   
+	mov x2, 220   
+	mov x3, 40    
+	bl cuadrado
+
+	//techo 2
+	mov x0, 220   
+	mov x1, 340   
+	mov x2, 240   
+	mov x3, 40    
+	bl cuadrado
+//gato------------------------------------------------------------------------------------------------------------
+	//cuerpo----------------------------------------
+	movz w12, 0x00, lsl 16
+	movk w12, 0x0000, lsl 00
+	mov x0, 140   
+	mov x1, 260  
+	mov x2, 40   
+	mov x3, 20    
+	bl cuadrado
+	
+	//cola-------------------------------------------
+	mov x0, 130   
+	mov x1, 270  
+	mov x2, 10   
+	mov x3, 5    
+	bl cuadrado
+
+	mov x0, 130   
+	mov x1, 260  
+	mov x2, 5   
+	mov x3, 10    
+	bl cuadrado
+
+	//cabeza-----------------------------------------
+	mov x0, 160   
+	mov x1, 240  
+	mov x2, 20   
+	mov x3, 20    
+	bl cuadrado
+
+	//orejas-----------------------------------------
+	mov x0, 160   
+	mov x1, 230  
+	mov x2, 3   
+	mov x3, 10    
+	bl cuadrado
+
+	mov x0, 162   
+	mov x1, 235  
+	mov x2, 3   
+	mov x3, 5    
+	bl cuadrado
+
+	mov x0, 174   
+	mov x1, 235  
+	mov x2, 3   
+	mov x3, 5    
+	bl cuadrado
+
+	mov x0, 177   
+	mov x1, 230  
+	mov x2, 3   
+	mov x3, 10    
+	bl cuadrado
+	
+	//ojos--------------------------------------------
+	movz w12, 0x00, lsl 16
+	movk w12, 0xCC66, lsl 00
+	mov x0, 164   
+	mov x1, 250  
+	mov x2, 5   
+	mov x3, 5    
+	bl cuadrado
+
+	mov x0, 174   
+	mov x1, 250  
+	mov x2, 5   
+	mov x3, 5    
+	bl cuadrado
+
+//---------------------------------------------------------------------------------------------------------------------------------------------------
+	//ventana1-------------------------------------------
 	movz w12, 0xFF, lsl 16
-	movk w12, 0xFFFF, lsl 00
+	movk w12, 0xCD00, lsl 00
 	mov x0, 560   //x
 	mov x1, 80   //y
 	mov x2, 80   //ancho
 	mov x3, 80    //alto
 	bl cuadrado
 	
-	//ventana2
+	//ventana2------------------------------------------
 	mov x0, 560   //x
 	mov x1, 240   //y
 	mov x2, 80   //ancho
 	mov x3, 80    //alto
 	bl cuadrado
 	
-	//ventana3
+	//ventana3-----------------------------------------
 	mov x0, 560   //x
 	mov x1, 400   //y
 	mov x2, 80   //ancho
 	mov x3, 80    //alto
 	bl cuadrado
-	
-	// Ejemplo de uso de gpios
-	mov x9, GPIO_BASE
 
-	// Atención: se utilizan registros w porque la documentación de broadcom
-	// indica que los registros que estamos leyendo y escribiendo son de 32 bits
+//---------------------------------------------------------------------------------------------------------------------------------------------
+	//cartel----------------------------------------------------
+	movz w12, 0x33, lsl 16
+	movk w12, 0x99FF, lsl 00
+	mov x0, 0    
+	mov x1, 350   
+	mov x2, 190   
+	mov x3, 110    
+	bl cuadrado
 
-	// Setea gpios 0 - 9 como lectura
-	str wzr, [x9, GPIO_GPFSEL0]
+	//Odc2025--------------------------------------------------
+	movz w12, 0x00, lsl 16
+	movk w12, 0x0000, lsl 00
+	mov x0, 5    
+	mov x1, 360   
+	mov x2, 30   
+	mov x3, 40    
+	bl cuadrado
 
-	// Lee el estado de los GPIO 0 - 31
-	ldr w10, [x9, GPIO_GPLEV0]
+	mov x0, 40   
+	mov x1, 360   
+	mov x2, 20   
+	mov x3, 40    
+	bl cuadrado
+    
+	mov x0, 65   
+	mov x1, 360   
+	mov x2, 30   
+	mov x3, 40    
+	bl cuadrado
 
-	// And bit a bit mantiene el resultado del bit 2 en w10
-	and w11, w10, 0b1000
+	mov x0, 5    
+	mov x1, 410   
+	mov x2, 30   
+	mov x3, 40    
+	bl cuadrado
 
-	// w11 será 1 si había un 1 en la posición 2 de w10, si no será 0
-	// efectivamente, su valor representará si GPIO 2 está activo
-	lsr w11, w11, 2
+	mov x0, 40   
+	mov x1, 410   
+	mov x2, 30   
+	mov x3, 40    
+	bl cuadrado
 
-	//---------------------------------------------------------------
-	// Infinite Loop
+	mov x0, 75   
+	mov x1, 410   
+	mov x2, 30   
+	mov x3, 40    
+	bl cuadrado
 
+	mov x0, 110  
+	mov x1, 410   
+	mov x2, 30   
+	mov x3, 40 
+	bl cuadrado
+
+	movz w12, 0x33, lsl 16
+	movk w12, 0x99FF, lsl 00
+	mov x0, 10
+	mov x1, 365
+	mov x2, 20
+	mov x3, 30
+	bl cuadrado
+
+	mov x0, 40
+	mov x1, 360
+	mov x2, 15
+	mov x3, 20
+	bl cuadrado
+
+	mov x0, 45
+	mov x1, 385
+	mov x2, 10
+	mov x3, 10
+	bl cuadrado
+
+	mov x0, 70
+	mov x1, 365
+	mov x2, 25
+	mov x3, 30
+	bl cuadrado
+
+	mov x0, 5    
+	mov x1, 415   
+	mov x2, 25   
+	mov x3, 12    
+	bl cuadrado
+
+	mov x0, 10    
+	mov x1, 433   
+	mov x2, 25   
+	mov x3, 12    
+	bl cuadrado
+
+	mov x0, 45   
+	mov x1, 415 
+	mov x2, 20
+	mov x3, 30
+	bl cuadrado
+
+	mov x0, 75   
+	mov x1, 415  
+	mov x2, 25   
+	mov x3, 12    
+	bl cuadrado
+
+	mov x0, 80    
+	mov x1, 433   
+	mov x2, 25   
+	mov x3, 12    
+	bl cuadrado
+
+	mov x0, 115   
+	mov x1, 415  
+	mov x2, 25   
+	mov x3, 12    
+	bl cuadrado
+
+	mov x0, 110  
+	mov x1, 433   
+	mov x2, 25   
+	mov x3, 12    
+	bl cuadrado
+
+//----------------------------------------------------------------------------------------------------------------------------------------------
 InfLoop:
 	b InfLoop
 //----------------------------------------------------------------------------
 fondo:
-	stp x5, x6, [sp,#16]
+	stp x5, x6, [sp,#-16]
 	mov x5, sp
 	mov x0,x20
 	mov x1,x21
 	mov x2,x22
-
-	mov x3, x2                   // Y Size
-loop1:
+salto_def:
+	mov x3, 40                 // Y Size
+c_1_1:
 	mov x4, x1 			      // X Size
-loop0:
+c_1_0:
 	stur w12,[x0]  // Colorear el pixel N
 	add x0,x0,4	   // Siguiente pixel
 	sub x4,x4,1	   // Decrementar contador X
-	cbnz x4, loop0  // Si no terminó la fila, salto
-
+	cbnz x4, c_1_0  // Si no terminó la fila, salto
 	sub x3,x3,1	   // Decrementar contador Y
-	sub w15, w12, 0x0001ff
-	cbnz w15, color
+	cbnz x3, c_1_1  // Si no es la última fila, salto
+	mov x3, 40
+	mov x11, 1
+salto1:
+	mov x10, 2
+i_1_1:
+	mov x4, x21
+i_1_0:
+	subs x7,x11,x10
+	b.ge else1
+	stur w12,[x0]
+	add x0,x0,4
+	sub x4,x4,1
+	cbnz x4, i_1_0
+	sub x10,x10,1	   
+	cbnz x10, i_1_1 
+	sub x3,x3,2
+	cbnz x3,salto1 
+else1:
+	stur w13,[x0]  
+	add x0,x0,4	   
+	sub x4,x4,1	   
+	cbnz x4, i_1_0 
+	sub x10,x10,1	   
+	cbnz x10, i_1_1  
+	sub x3,x3,2
+	cbnz x3,salto1
+	add w12, w12, 11
+	add w13, w13, 11
+	sub x2, x2, 80
+	cbnz x2, salto_def
 
-	cbnz x3, loop1  // Si no es la última fila, salto
 	ldp x5, x6, [sp], 16
 	ret
-
-//-------------------------------------------------------------------
-color:
-	add w12,w12,1
-	b loop1
+//--------------------------------------------------------------------------------------------------------------------------------------------
 cuadrado:
 	stp x5, x6, [sp, #-16]
 	stp x7, x8, [sp, #-16]
+
 	
 	mov x5, x0   // x
 	mov x6, x1   // y
@@ -201,7 +427,7 @@ filas:
 	mul x10, x6, x21   // y * ancho
 	add x10, x10, x5   // + x
 	lsl x10, x10, 2    // *4
-	add x10, x20, x10  // dirección base
+	add x10, x20, x10  // dirección base. Sirve para dar ubicarno en el pixel de arranque
 
 columnas:
 	stur w12, [x10]
@@ -217,4 +443,46 @@ columnas:
 	ldp x5, x6, [sp], 16
 	ret
 //--------------------------------------------------
- 
+circulo:
+	stp x3, x4, [sp, #-16]
+	str x5, [sp]
+	
+	mov x3, x0         // x
+	mov x4, x1         // y
+	mov x5, x2         // radio
+	mul x6, x5, x5     //radio^2
+
+	sub x7, x3, x5       //x-radio
+	add x8, x3, x5       //x+radio
+	
+eje_y:
+	sub x9, x4, x5      //y-radio
+	add x10, x4, x5     //y+radio
+eje_x:
+	sub x0, x3, x7
+	sub x1, x4, x9
+	mul x11, x0, x0
+	mul x12, x1, x1
+	add x13, x11, x12     //x'^2+y'^2
+	
+	cmp x13, x6
+	b.gt saltear
+
+	mul x0, x9, x21
+	add x0, x0, x7
+	lsl x0, x0, 2
+	add x0,x20, x0
+	stur x29,[x0]
+saltear:
+	add x9, x9, 1
+	cmp x9, x10
+	ble eje_x
+	add x7,x7,1
+	cmp x7,x8
+	ble eje_y
+
+	ldp x3,x4,[sp],16
+	ldr x5, [sp]
+
+	ret
+//---------------------------------------------------------
