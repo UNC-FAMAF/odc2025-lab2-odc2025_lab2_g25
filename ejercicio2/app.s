30183d8a7bbb45bfafbcf08fc65a8619271153d3
josefinarys01 <josefinareyesp4@gmail.com>
2025-06-06 15:21:39 -0300

    Principal con las llamadas a funciones animadas

--- ejercicio2/app.s
+++ ejercicio2/app.s
@@ -1,53 +1,128 @@
 	.equ SCREEN_WIDTH, 		640
 	.equ SCREEN_HEIGH, 		480
 	.equ BITS_PER_PIXEL,  	32
-
-	.equ GPIO_BASE,      0x3f200000
-	.equ GPIO_GPFSEL0,   0x00
-	.equ GPIO_GPLEV0,    0x34
-
 	.globl main
 
 main:
 	// x0 contiene la direccion base del framebuffer
  	mov x20, x0	// Guarda la dirección base del framebuffer en x20
-	//---------------- CODE HERE ------------------------------------
+	//----------------codigos------------------------------------
+	mov x21,SCREEN_WIDTH
+	mov x22,SCREEN_HEIGH
 
-	movz x10, 0xC7, lsl 16
-	movk x10, 0x1585, lsl 00
+buclecin: 
 
-	mov x2, SCREEN_HEIGH         // Y Size
-loop1:
-	mov x1, SCREEN_WIDTH         // X Size
-loop0:
-	stur w10,[x0]  // Colorear el pixel N
-	add x0,x0,4	   // Siguiente pixel
-	sub x1,x1,1	   // Decrementar contador X
-	cbnz x1,loop0  // Si no terminó la fila, salto
-	sub x2,x2,1	   // Decrementar contador Y
-	cbnz x2,loop1  // Si no es la última fila, salto
+	mov x26, 0
+	mov x27, 0
+	mov x25, 30
+ciclo :  //Se acerca a la orilla
+	bl fondo
+	bl gato 
+	bl delay
+	add x26, x26, 1
+	sub x25, x25, 1
+	cbnz x25, ciclo
 
-	// Ejemplo de uso de gpios
-	mov x9, GPIO_BASE
+	mov x24, -5
+	mov x25, 10
+ciclo2 : //Salta
+	bl fondo
+	bl gato 
+	bl delay
+	add x26, x26, 5
+	bl parabola
+	add x24, x24, 1
+	sub x25, x25, 1
+	cbnz x25, ciclo2
 
-	// Atención: se utilizan registros w porque la documentación de broadcom
-	// indica que los registros que estamos leyendo y escribiendo son de 32 bits
+	mov x25, 50
+ciclo3 : //Baja
+	bl fondo
+	bl gato 
+	bl delay
+	add x26, x26, 2
+	add x27, x27, 3
+	sub x25, x25, 1
+	cbnz x25, ciclo3
 
-	// Setea gpios 0 - 9 como lectura
-	str wzr, [x9, GPIO_GPFSEL0]
+	mov x25, 50
+ciclo4 : //Se acerca a la otra orilla
+	bl fondo
+	bl gato 
+	bl delay
+	add x26, x26, 2
+	sub x25, x25, 1
+	cbnz x25, ciclo4
 
-	// Lee el estado de los GPIO 0 - 31
-	ldr w10, [x9, GPIO_GPLEV0]
+	mov x24, -5
+	mov x25, 10
+ciclo5 : //Salta
+	bl fondo
+	bl gato 
+	bl delay
+	add x26, x26, 5
+	bl parabola
+	add x24, x24, 1
+	sub x25, x25, 1
+	cbnz x25, ciclo5
 
-	// And bit a bit mantiene el resultado del bit 2 en w10
-	and w11, w10, 0b10
+	mov x25, 23
+ciclo6 : //Cae en el balcon
+	bl fondo
+	bl gato 
+	bl delay
+	add x26, x26, 2
+	add x27, x27, 3
+	sub x25, x25, 1
+	cbnz x25, ciclo6
 
-	// w11 será 1 si había un 1 en la posición 2 de w10, si no será 0
-	// efectivamente, su valor representará si GPIO 2 está activo
-	lsr w11, w11, 1
+	mov x25, 50
+ciclo7 : //Se va
+	bl fondo
+	bl gato 
+	bl delay
+	add x26, x26, 2
+	sub x25, x25, 1
+	cbnz x25, ciclo7
 
+	mov x24, -5
+	mov x25, 10
+ciclo8 : //Salta
+	bl fondo
+	bl gato 
+	bl delay
+	add x26, x26, 5
+	bl parabola
+	add x24, x24, 1
+	sub x25, x25, 1
+	cbnz x25, ciclo8
+
+	mov x25, 12
+ciclo9 : //Cae en el techo
+	bl fondo
+	bl gato 
+	bl delay
+	add x26, x26, 2
+	add x27, x27, 3
+	sub x25, x25, 1
+	cbnz x25, ciclo9
+
+	mov x25, 45
+ciclo10 : //Se pone en loop
+	bl fondo
+	bl gato 
+	bl delay
+	add x26, x26, 2
+	sub x25, x25, 1
+	cbnz x25, ciclo10
+
+    b buclecin
 	//---------------------------------------------------------------
 	// Infinite Loop
-
 InfLoop:
 	b InfLoop
+
+
+
+//--------------------------------------------------
+
